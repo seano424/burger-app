353ade16c90cea73645fd8511051ff153b0e1d2d
Sean OReilly <soreilly424@gmail.com>
2021-01-14 15:27:45 -0500

    btns

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./Button.module.css"
+
+const button = (props) => (
+  <button className={[styles.Button, styles[props.btnType]]} onClick={props.clicked}>{props.children}</button>
+)
+
+export default button
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,14 @@
 import React from "react"
 import styles from "./Modal.module.css"
+import Backdrop from "../Backdrop/Backdrop"
 
 const modal = (props) => (
+  <>
+  <Backdrop show={props.show} clicked={props.modalClosed} />
   <div style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}} className={styles.Modal}>
     {props.children}
   </div>
+  </>
 )
 
 export default modal;

3a4e89ca2dbb076d3a43481d1b5f586fd3004ca8
Sean OReilly <soreilly424@gmail.com>
2021-01-14 16:44:49 -0500

    sidedrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from "./Toolbar.module.css"
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
     <div>Menu</div>
-    <Logo />
+    <div className={styles.Logo}>
+      <Logo />
+    </div>
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 )
